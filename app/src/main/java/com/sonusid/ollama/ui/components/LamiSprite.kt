545d66c86fb1fa3e2c193e1ccb5723970a5d587e
ninbyo02 <ninbyo02@gmail.com>
2025-12-29 08:53:43 +0900

    chore: add compose getValue import

--- app/src/main/java/com/sonusid/ollama/ui/components/LamiSprite.kt
+++ app/src/main/java/com/sonusid/ollama/ui/components/LamiSprite.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
