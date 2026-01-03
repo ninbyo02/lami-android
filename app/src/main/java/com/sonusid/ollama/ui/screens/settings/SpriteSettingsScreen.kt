ee5648b983ac934509f557794aa3b901eb036235
ninbyo02 <ninbyo02@gmail.com>
2026-01-04 01:05:13 +0900

    fix: restore imePadding import (#415)

--- app/src/main/java/com/sonusid/ollama/ui/screens/settings/SpriteSettingsScreen.kt
+++ app/src/main/java/com/sonusid/ollama/ui/screens/settings/SpriteSettingsScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.ime
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
