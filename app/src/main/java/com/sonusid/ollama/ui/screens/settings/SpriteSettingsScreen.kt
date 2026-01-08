850cdbf0600f75a1888f31cee55885ead602dfde
ninbyo02 <ninbyo02@gmail.com>
2026-01-08 21:31:55 +0900

    fix: add WindowInsets companion imports (#499)

--- app/src/main/java/com/sonusid/ollama/ui/screens/settings/SpriteSettingsScreen.kt
+++ app/src/main/java/com/sonusid/ollama/ui/screens/settings/SpriteSettingsScreen.kt
@@ -21,11 +21,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsets.Companion.ime
+import androidx.compose.foundation.layout.WindowInsets.Companion.systemBars
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.layout.offset
