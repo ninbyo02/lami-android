61034f076fc1b5f7c29120c918d3735fe2fa0c35
ninbyo02 <ninbyo02@gmail.com>
2026-02-08 21:27:18 +0900

    fix: update About to use app name and build version (#1021)

--- app/src/main/java/com/sonusid/ollama/ui/screens/settings/Settings.kt
+++ app/src/main/java/com/sonusid/ollama/ui/screens/settings/Settings.kt
@@ -58,6 +58,7 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -657,7 +658,10 @@ fun Settings(navgationController: NavController, onSaved: () -> Unit = {}) {
                         HorizontalDivider()
                         ListItem(
                             headlineContent = {
-                                Text("About", style = MaterialTheme.typography.titleMedium)
+                                Text(
+                                    stringResource(R.string.about),
+                                    style = MaterialTheme.typography.titleMedium
+                                )
                             },
                             supportingContent = {
                                 Text(
